9215803391a84b8095460612f4651b273fe0aba9
gabriel <gabriell_rsilva@outlook.com>
2025-05-25 17:01:53 -0300

    feat: editable-dataframe

--- src/main.py
+++ src/main.py
@@ -6,16 +6,60 @@ import os
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💰", layout="wide")
 
+category_file = "categories.json"
+
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": []
+    }
+
+
+if os.path.exists(category_file):
+    with open(category_file, "r") as f:
+        st.session_state.categories = json.load(f)
+
+
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+
+
+def categorize_transactions(df):
+    df["Category"] = "Uncategorized"
+
+    for category, keywords in st.session_state.categories.items():
+        if category == "Uncategorized" or not keywords:
+            continue
+        
+        lowered_keywords = [keyword.lower().strip() for keyword in keywords]
+
+        for idx, row in df.iterrows():
+            details = row["Details"].lower().strip()
+            if details in lowered_keywords:
+                df.at[idx, "Category"] = category
+
+    return df        
+
+
 def load_transactions(file) -> pd.DataFrame:
     try:
         df = pd.read_csv(file)
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         st.dataframe(df, hide_index=True )
-        return df
+        return categorize_transactions(df)
     
     except Exception as e:
         st.error(f"Error process file: {str(e)}")
+
+
+def add_keyword_to_category(category, keyword) -> bool:
+    keyword = keyword.strip()
+    if keyword and keyword not in st.session_state.categories[category]:
+        st.session_state.categories[category].append(keyword)
+        save_categories()
+        return True
+    return False
 
 
 def main():
@@ -32,10 +76,48 @@ def main():
             debits_df = df[df["Debit/Credit"] == "Debit"].copy()
             credits_df = df[df["Debit/Credit"] == "Credit"].copy()
 
+            st.session_state.debits_df = debits_df.copy()
+
             tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
 
             with tab1:
-                st.dataframe(debits_df, hide_index=True)
+
+                new_category = st.text_input("New Category Name")
+                add_button = st.button("Add Category", type="primary")
+
+                if add_button and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.rerun()
+
+                st.subheader("Your Expenses")
+
+                edited_df = st.data_editor(
+                    st.session_state.debits_df[["Date", "Details", "Amount", "Category"]],
+                    column_config={
+                        "Date": st.column_config.DateColumn("Date", format="DD/MM/YYYY"),
+                        "Amount": st.column_config.NumberColumn("Amount", format="%.2f BRL"),
+                        "Category": st.column_config.SelectboxColumn(
+                            "Category",
+                            options=list(st.session_state.categories.keys())
+                        )
+                    },
+                    hide_index=True,
+                    key="category_editor"
+                )
+
+                save_button = st.button("Apply Changes", type="primary")
+                if save_button:
+                    for idx, row in edited_df.iterrows():
+                        new_category = row["Category"]
+                        if new_category == st.session_state.debits_df.at[idx, "Category"]:
+                            continue
+
+                        details = row["Details"]
+                        st.session_state.debits_df.at[idx, "Category"] = new_category
+                        add_keyword_to_category(new_category, details)
+
 
             with tab2:
                 st.dataframe(credits_df, hide_index=True)
